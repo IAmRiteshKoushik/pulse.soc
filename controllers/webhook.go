449305ff77c4848c60d2375dfdcf5f84acefdd1b
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-30 07:38:09 +0530

    fix: Webhook json binding

--- controllers/webhook.go
+++ controllers/webhook.go
@@ -14,8 +14,8 @@ import (
 
 // WebhookRequest
 type WebhookRequest struct {
-	FirstName string `json:"first_name" binding:"required"`
-	LastName  string `json:"last_name" binding:"required"`
+	FirstName string `json:"firstName" binding:"required"`
+	LastName  string `json:"lastName" binding:"required"`
 	Email     string `json:"email" binding:"required,email"`
 	Password  string `json:"password" binding:"required"`
 }
